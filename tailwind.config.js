f028ec18004d8d3d82581eb778977c36abfa1c6f
Hristijan Gjorgjiev <gorg1ev@protonmail.com>
2023-03-19 03:43:21 +0100

    added hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,11 @@ module.exports = {
    content: ['./src/**/*.{html,js}'],
    theme: {
       screens: {
+         xs: '375px',
          sm: '480px',
          md: '768px',
          lg: '976px',
-         xl: '1440',
+         xl: '1109px',
       },
       extend: {
          colors: {
